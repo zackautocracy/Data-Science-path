48f07e5464bb7d6d2e817b8c7c513f2aeb986dbe
zackID <codeautocracy@gmail.com>
2020-04-04 20:33:44 +0100

    Starting my second SQL project
    world population II:
    - working with multiple tables
    - using aggregate function

---
+++ SQLprojects/world-population-II/population_queries.sql
@@ -0,0 +1,27 @@
+-- How many entries in the database are from Africa?
+
+
+
+-- What was the total population of Oceania in 2005?
+
+
+
+-- What is the average population of countries in South America in 2003?
+
+
+
+-- What country had the smallest population in 2007?
+
+
+
+-- What is the average population of Poland during the time period covered by this dataset?
+
+
+
+-- How many countries have the word "The" in their name?
+
+
+
+-- What was the total population of each continent in 2010?
+
+
